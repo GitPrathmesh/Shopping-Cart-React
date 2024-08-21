4d4764713f4a60e09d2b84b51871bad56a10d595
GitPrathmesh <mailyourprathmesh@gmail.com>
2024-08-21 14:48:22 +0530

    added basename in App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -10,7 +10,7 @@ function App() {
   return(<>
     <div className="App">
       <ShopContextProvider>
-        <Router>
+        <Router basename="/Shopping-Cart-React">
           <Navbar />
           <Routes>
             <Route path="/" element={<Shop />} />
